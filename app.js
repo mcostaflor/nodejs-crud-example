671809c12551aedcba6ca02a2676e8d3a05698b6
Matheus Costa <matheuscostaflor@gmail.com>
2019-10-08 11:20:08 -0300

    Init Express

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/', require('./routes/index'));
+
+module.exports = app;
